97dbac46c178aad2c4996f9db38295f62aba5c58
João Vitor Ibiapino <ibiapinojoaovitor@gmail.com>
2025-07-22 21:59:21 -0300

    data access layer

--- src/main/java/br/com/dio/persistence/entity/BoardColumnEntity.java
+++ src/main/java/br/com/dio/persistence/entity/BoardColumnEntity.java
@@ -4,8 +4,10 @@ import lombok.Data;
 
 @Data
 public class BoardColumnEntity {
+
     private Long id;
     private String name;
     private int order;
     private BoardColumnKindEnum kind;
+    private BoardEntity board = new BoardEntity();
 }
---
+++ src/main/java/br/com/dio/service/BoardQueryService.java
@@ -0,0 +1,36 @@
+package br.com.dio.service;
+
+import br.com.dio.persistence.dao.BoardColumnDAO;
+import br.com.dio.persistence.dao.BoardDAO;
+import br.com.dio.persistence.entity.BoardColumnEntity;
+import br.com.dio.persistence.entity.BoardEntity;
+import lombok.AllArgsConstructor;
+
+import javax.swing.text.html.parser.Entity;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Optional;
+
+@AllArgsConstructor
+public class BoardQueryService {
+
+    private final Connection connection;
+
+    public Optional<BoardEntity> findById(final Long id) throws SQLException {
+        var dao = new BoardDAO(connection);
+        var boardColumnDAO = new BoardColumnDAO(connection)
+        var optinal = dao.findById(id);
+        if(optinal.isPresent()){
+            var entity = optinal.get();
+            entity.setBoardColumns(boardColumnDAO.findByBoardId(Entity.getId()));
+            return Optional.of(entity);
+        }
+        return Optional.empty();
+    }
+
+    public List<BoardColumnEntity> findByBoardId(final Long id) throws SQLException{
+        return null;
+    }
+
+}
