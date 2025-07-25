f2df71bf9f090b6988ebed52ede24ae7efff4685
João Vitor Ibiapino <ibiapinojoaovitor@gmail.com>
2025-07-25 03:11:46 -0300

    error treatment

--- src/main/java/br/com/dio/ui/BoardMenu.java
+++ src/main/java/br/com/dio/ui/BoardMenu.java
@@ -1,5 +1,6 @@
 package br.com.dio.ui;
 
+import br.com.dio.dto.BoardColumnInfoDTO;
 import br.com.dio.persistence.entity.BoardColumnEntity;
 import br.com.dio.persistence.entity.BoardEntity;
 import br.com.dio.persistence.entity.CardEntity;
@@ -66,7 +67,17 @@ public class BoardMenu {
         }
     }
 
-    private void moveCardToNextColmun() {
+    private void moveCardToNextColmun() throws SQLException {
+        System.out.println("Informe o id do card que deseja mover para a próxima coluna:");
+        var cardId = scanner.nextLong();
+        var boardColumnsInfo = entity.getBoardColumns().stream()
+                .map(bc -> new BoardColumnInfoDTO(bc.getId(), bc.getOrder(), bc.getKind()))
+                .toList();
+        try(var connection = getConnection()){
+            new CardService(connection).moveToNextColumn(cardId, boardColumnsInfo);
+        } catch (RuntimeException ex){
+            System.out.println(ex.getMessage());
+        }
     }
 
     private void blockCard() {
